0e703910bf8256eda9a0138dae8f1b3181c24f5e
sravya-oruganti <osravs@gmail.com>
2021-09-15 12:19:40 +0530

    third one

---
+++ src/app/Components/mycharts/mycharts.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MychartsComponent } from './mycharts.component';
+
+describe('MychartsComponent', () => {
+  let component: MychartsComponent;
+  let fixture: ComponentFixture<MychartsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MychartsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MychartsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MychartsComponent } from './Components/mycharts/mycharts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MychartsComponent
   ],
   imports: [
     BrowserModule,
